371bcf10cba3ca1fcf7bff0c9fc249b1378c8f4f
Aniruddha Dewangan <ani.dewangan2002@gmail.com>
2024-06-04 18:50:05 +0530

    greedy_02

--- 18_Greedy/02_activitySelection.cpp
+++ 18_Greedy/02_activitySelection.cpp
@@ -1,33 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int naiveSolution(int arr[], int n)
+bool compare(pair<int, int> a, pair<int, int> b)
 {
-     int res=0;
-     // while (All items are not considered)
-     // {
-     //      i=selectAnItem()
-     //      if (feasible(i))
-     //           res=res+i;
-     // }
+     return (a.second<b.second);
+}
+
+int maxActivities(pair <int, int> arr[], int n)
+{
+     sort(arr, arr+n, compare);
+     int res=1;
+     int prev=0;
+
+     for (int current=1; current<n; ++current)
+     {
+          if (arr[current].first>=arr[prev].second)
+          {
+               res+=1;
+               prev=current;
+          }
+     }
 
      return res;
 }
 
+
 int main()
 {
-     vector<vector<int>> mat;
-     mat[0][0]=3;
-     mat[0][1]=8;
-     mat[1][0]=2;
-     mat[1][1]=4;
-     mat[2][0]=1;
-     mat[2][1]=3;
-     mat[3][0]=10;
-     mat[3][1]=11;
+     int n=3;
+     pair<int, int> arr[n]={{12,25}, {10,20}, {20,30}};
 
+     int res=maxActivities(arr, n);
+     cout<<res;
      
-     
-
      return 0;
 }
