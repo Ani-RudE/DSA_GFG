20cf4bbbe6f03e53e8a1af27020478237846f53f
Aniruddha Dewangan <ani.dewangan2002@gmail.com>
2024-06-27 13:07:47 +0530

    speed-cp

--- [STL]/00_speedHack.cpp
+++ [STL]/00_speedHack.cpp
@@ -6,13 +6,15 @@ using namespace std;
 // Speed optimization
 auto speedHack = []()
 {
-     ios_base::sync_with_stdio(false);
-     cin.tie(nullptr);
-     cout.tie(nullptr);
-     return 0;
+ios_base::sync_with_stdio(false);
+cin.tie(nullptr);
+cout.tie(nullptr);
+return 0;
 }();
 
 /* ****************************** */
 
 #pragma GCC optimize("Ofast")
 #pragma GCC optimize("O3")
+
+#pragma pack(1) // For struct to remove struct size padding
